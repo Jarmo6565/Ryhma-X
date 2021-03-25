c3aa842a01156118056bdd0d090ab1853bb7a61b
Leevi12 <leevi.sallantaus@gmail.com>
2021-03-25 16:52:24 +0200

    Sain javascript osuuden valmiiksi!/Leevi/25.3.2021

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 function myFunction() {
-  var nimi = document.getElementById("etunimi").value
-  var sukunimi = document.getElementById("sukunimi").value;
-  var unijoo = document.getElementById("unijoo");
-  var uniei = document.getElementById("uniei");
+  var name = document.getElementById("etunimi").value;
+  var lastname = document.getElementById("sukunimi").value;
+  var sleepyes = document.getElementById("unijoo");
+  var sleepno = document.getElementById("uniei");
+  var car1 = document.getElementById("kulku1");
+  var car2 = document.getElementById("kulku2");
+  var car3 = document.getElementById("kulku3");
+  var feedback = document.getElementById("palaute").value;
 
-  if(nimi == "") {
+  if(name == "") {
   alert("Syötä nimesi");
   return false;
-} else if(sukunimi == "") {
-alert("Syötä sukunimesi");
-return false;
-} else if(unijoo.checked == false && uniei.checked == false) {
+} else if(lastname == "") {
+  alert("Syötä sukunimesi");
+  return false;
+} else if(sleepyes.checked == false && sleepno.checked == false) {
   alert("Valitse nukutko öisin hyvin");
   return false;
+} else if(car1.checked == false && car2.checked == false && car3.checked == false) {
+  alert("Valitse käyttämäsi kulkuneuvot");
+  return false;
+} else if(feedback == "") {
+  alert("Ole hyvä ja anna meille palautetta");
+  return false;
+} else {
+  alert("Kiitos vastauksestasi!")
 }
 }

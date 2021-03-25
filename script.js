265ec473f9de1001ba35befaaa27993b5a822f6a
Leevi12 <leevi.sallantaus@gmail.com>
2021-03-25 16:56:12 +0200

    Lisäsin vielä kommentit/Leevi/25.3.2021

--- script.js
+++ script.js
@@ -8,22 +8,28 @@ function myFunction() {
   var car3 = document.getElementById("kulku3");
   var feedback = document.getElementById("palaute").value;
 
+//Jos nimi on tyhjä
   if(name == "") {
   alert("Syötä nimesi");
   return false;
+  //Jos sukunimi on tyhjä
 } else if(lastname == "") {
   alert("Syötä sukunimesi");
   return false;
+  //Jos nukutko hyvin ei ole vastattu mitään
 } else if(sleepyes.checked == false && sleepno.checked == false) {
   alert("Valitse nukutko öisin hyvin");
   return false;
+  //Jos kulkuneuvoa ei ole valittu
 } else if(car1.checked == false && car2.checked == false && car3.checked == false) {
   alert("Valitse käyttämäsi kulkuneuvot");
   return false;
+  //Jos palautetta ei ole annettu
 } else if(feedback == "") {
   alert("Ole hyvä ja anna meille palautetta");
   return false;
+  //Kun kaikkeen on vastattu tulee kiitos viesti
 } else {
-  alert("Kiitos vastauksestasi!")
+  alert("Kiitos vastauksestasi!");
 }
 }

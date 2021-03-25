f6aef4e02da969e76473b47c6b2a3c3e9b3b0133
jarmo6565 <pjarmo75@gmail.com>
2021-03-25 21:11:09 +0200

    Css asetuksien laitto. Vielä on fiilattavaa!!  25.3.2021

---
+++ scripti.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var nimi = document.getElementById("nimi").value
+  var sukunimi = document.getElementById("sukunimi").value;
+
+  if(nimi == "") {
+  alert("Syötä nimesi");
+  return false;
+} else if(sukunimi == "") {
+alert("Syötä sukunimesi");
+return false;
+}
+}
+//********************************

1e3329addd05153e724a764afccb7d937a34992f
jarmo6565 <pjarmo75@gmail.com>
2021-04-04 17:47:59 +0300

    Tähän yritin saada alert:ia nimi laatikkoon. Siitä on kopio ohjelmassa.Ohjelma toimii normaalisti.

--- Projekti_1B/script.js
+++ Projekti_1B/script.js
@@ -3,13 +3,24 @@ function myFunction(){
   var name = document.getElementById('nimi').value;
   var sahkoposti = document.getElementById('email').value;
   var age = parseInt(document.getElementById('ika').value);
+  var name;
 
+/*
+
+<p id="name"></p>
+  if (confirm("Do you want to save changes?") == true) {
+    userPreference = "Data saved successfully!";
+  } else {
+      userPreference = "Save Cancelled!";
+  }
+document.getElementById("name").innerHTML = userPreference;
+*/
 
   //Nimikenttä
   if (name.length <= 5){
     document.getElementById('joslyhyt').innerHTML = "Antamasi nimi on liian lyhyt";
     return false;
-}
+
   //S-postikenttä
   function emailIsValid (email){
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)

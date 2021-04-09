9184c8e63ba62f7befc1ee94f0d89175d61b9627
Leevi12 <leevi.sallantaus@gmail.com>
2021-04-09 14:12:45 +0300

    Tein nyt vähän matkaa koodia mutta mikään ei toimi eikä ole vielä liitoksissa mihinkään

--- Projekti_2/script.js
+++ Projekti_2/script.js
@@ -1,3 +1,55 @@
-var random = Math.floor(Math.random() * 10 + 1);
-var arvaus = 1;
-var x  = document.getElementById("kayttajanArvaus").value;
+function numeroVoittaja(min, max) {
+  min = 1;
+  max = 10;
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+class Game {
+
+  constructor() {
+    // Pelaajan nykyinen arvaus
+    this.pelaajanArvaus = null;
+    // Pitää sisällään pelaajan tähänastiset arvaukset
+    this.arvaukset = []
+    // Arpoo meille voittavan numeron
+    this.voittoNumero = numeroVoittaja()
+  }
+
+  // methods
+  difference() {
+    return Math.abs(this.pelaajanArvaus - this.voittoNumero)
+  }
+
+  isLower() {
+    if (this.pelaajanArvaus < this.voittoNumero) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+tarkistus() {
+
+    // Voitto
+    if (this.pelaajanArvaus === this.voittoNumero) {
+      return "Voitit pelin!";
+    }
+
+    // Häviö
+    if (this.arvaukset.length >= 3) {
+      return "Hävisit pelin";
+    }
+
+    // Numero on suurempi
+    if (Math.abs(this.pelaajanArvaus - this.voittoNumero) < 0) {
+      return "Numero on suurempi!"
+    }
+
+    // Numero on pienempi
+    if (Math.abs(this.pelaajanArvaus - this.voittoNumero) > 0) {
+      return "Numero on pienempi!"
+    }
+
+  }

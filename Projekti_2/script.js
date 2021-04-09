a0b1cf5aaac9562305adee2b508bca2e76f88f36
Leevi12 <leevi.sallantaus@gmail.com>
2021-04-09 10:56:21 +0300

    JS alku/Leevi/9.4.2021

---
+++ Projekti_2/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var but1 = document.getElementById("nappi1").value;
+  var but2 = document.getElementById("nappi2").value;
+  var but3 = document.getElementById("nappi3").value;
+  var but4 = document.getElementById("nappi4").value;
+  var but5 = document.getElementById("nappi5").value;
+  var but6 = document.getElementById("nappi6").value;
+  var but7 = document.getElementById("nappi7").value;
+  var but8 = document.getElementById("nappi8").value;
+  var but9 = document.getElementById("nappi9").value;
+  var but10 = document.getElementById("nappi10").value;
+}

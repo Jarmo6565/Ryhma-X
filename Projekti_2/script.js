20f92ae7615174b140d5c8c7d774cd13c65943a6
Leevi12 <leevi.sallantaus@gmail.com>
2021-04-12 14:53:58 +0300

    JS/Leevi&sanna/12.4.2021

--- Projekti_2/script.js
+++ Projekti_2/script.js
@@ -1,13 +1,70 @@
+var voittoNumero = Math.floor(Math.random()*10 + 1);
 
 function myFunction() {
 
+function arvauskone () {
+  if (document.getElementById('nappi1').clicked == true){
+  return 1;
+}
+  else if (document.getElementById('nappi2').clicked == true) {
+  return 2;
+}
+else if (document.getElementById('nappi3').clicked == true) {
+return 3;
+}
+else if (document.getElementById('nappi4').clicked == true) {
+return 4;
+}
+else if (document.getElementById('nappi5').clicked == true) {
+return 5;
+}
+else if (document.getElementById('nappi6').clicked == true) {
+return 6;
+}
+else if (document.getElementById('nappi7').clicked == true) {
+return 7;
+}
+else if (document.getElementById('nappi8').clicked == true) {
+return 8;
+}
+else if (document.getElementById('nappi9').clicked == true) {
+return 9;
+}
+else if (document.getElementById('nappi10').clicked == true) {
+return 10;
+}
 /* OOn laittanu tänne nyt erilaisia ideoita millä sen sais tehtyä
 pitää vaan tuumailla miten saa kaiken toimii ja miten saa
 laskettua ne käyttäjän klikkaukset ja samalla tarkistettua
 onko se oikea vastaus*/
-var pelaajanArvaus =
-var voittoNumero = Math.floor(Math.random()*10 + 1);
-var but1 = parseInt(document.getElementById("nappi1").value);
+var pelaajanArvaus = arvauskone();
+var arvaukset = 0;
+
+if (pelaajanArvaus === voittoNumero) {
+  document.getElementById("voitto").innerHTML = "Voitit Pelin!";
+}
+
+// Häviö
+else if (arvaukset >= 3) {
+  document.getElementById("havio").innerHTML = "Hävisit Pelin!";
+}
+
+// Numero on suurempi
+else if (pelaajanArvaus < voittoNumero) {
+  document.getElementById("suuri").innerHTML = "Numero on suurempi!";
+  arvaukset++;
+}
+
+// Numero on pienempi
+else {
+  document.getElementById("pieni").innerHTML = "Numero on pienempi!";
+  arvaukset++;
+}
+}
+}
+
+
+/*document.getElementById("nappi1").onclick;
 var but2 = parseInt(document.getElementById("nappi2").value);
 var but3 = parseInt(document.getElementById("nappi3").value);
 var but4 = parseInt(document.getElementById("nappi4").value);
@@ -17,12 +74,13 @@ var but7 = parseInt(document.getElementById("nappi7").value);
 var but8 = parseInt(document.getElementById("nappi8").value);
 var but9 = parseInt(document.getElementById("nappi9").value);
 var but10 = parseInt(document.getElementById("nappi10").value);
-var arvaukset = 0;
 
 
+console.log(pelaajanArvaus);
+}
     /*Tämä voi olla hyvä tapa ehkä?
     if (document.getElementById("nappi1").clicked == true) && (this.pelaajanArvaus === this.voittoNumero) {
-    */
+    */ /*
     but1.onclick = function klikkaus() {
     arvaukset++;
     disp.innerHTML = arvaukset;
@@ -89,7 +147,7 @@ var arvaukset = 0;
 
   }
 
-
+*/
 
 
 
